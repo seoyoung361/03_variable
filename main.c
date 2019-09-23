9e8b89d276cca0b88ed6a3dd2b92d95723715b6a
seoyoung361 <chris7400@naver.com>
2019-09-23 15:13:57 +0900

    02

--- main.c
+++ main.c
@@ -5,8 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello Sookmyung EE!\n");
+	int x;
+	short z;
+	float a;
+	double b;
 	
+	printf("bytes of int : %d\n", sizeof(x));
+	printf("bytes of short : %d\n", sizeof(z));
+	printf("bytes of float : %d\n", sizeof(a));
+	printf("bytes of double : %d\n", sizeof(b));
 	return 0;
 }
 
