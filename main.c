705fb0cd7812894b087523eaaaf698511139cf52
seoyoung361 <chris7400@naver.com>
2019-09-23 15:26:55 +0900

    03

--- main.c
+++ main.c
@@ -5,15 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x;
-	short z;
-	float a;
-	double b;
+	int input_int;
+	float input_float;
 	
-	printf("bytes of int : %d\n", sizeof(x));
-	printf("bytes of short : %d\n", sizeof(z));
-	printf("bytes of float : %d\n", sizeof(a));
-	printf("bytes of double : %d\n", sizeof(b));
+	printf("enter integer : ");
+	scanf("%d" , &input_int);
+	
+	printf("enter float : ");
+	scanf("%f" , &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float );
+
 	return 0;
 }
 
